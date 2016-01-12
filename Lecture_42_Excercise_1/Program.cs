77b4652d4617f91089902c74ad4efb17c2e14d18
Matt Kirchner <mattkirchner@live.com>
2016-01-12 15:42:23 -0600

    Finished Exercise 1

--- Lecture_42_Excercise_1/Program.cs
+++ Lecture_42_Excercise_1/Program.cs
@@ -10,6 +10,24 @@ namespace Lecture_42_Excercise_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number between 1 and 10");
+            var number = Int32.Parse(Console.ReadLine());
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine(Valid);
+            }
+            else
+            
+                Console.writeline(Invalid);
+
+
+            ShowExit();
+        }
+
+        static void Showexit()
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.WriteLine();
         }
     }
 }
